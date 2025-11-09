e820960fa496df39f6e8e8910d7d4b1e0cb65ece
yrh2 <syahirahas28@gmail.com>
2025-11-09 11:13:21 +0800

    Delete app directory

--- app/scanner.py
+++
@@ -1,9 +0,0 @@
-def scan_network():
-    return {
-        "open_ports": [80, 443],
-        "arp_spoofing": False,
-        "dns_spoofing": True,
-        "rogue_ap": False,
-        "threat_score": "Medium",
-        "recommendation": "Use VPN and avoid sensitive activities."
-    }
